4eff520739f87b10b97f0ab846c31d8cdf17bace
masterUNG <phrombutr@gmail.com>
2018-10-21 11:22:08 +0700

    onClick Success

--- app/src/main/java/masterung/androidthai/in/th/easycommand/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/easycommand/MainActivity.java
@@ -2,6 +2,8 @@ package masterung.androidthai.in.th.easycommand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,14 +16,31 @@ public class MainActivity extends AppCompatActivity {
 
     private String string = "Please Speak Command";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        showCommand(string);
+
+
+//        Mic Controller
+        micController();
 
     }   // Main Method
+
+    private void micController() {
+        ImageView imageView = findViewById(R.id.imvMicrophone);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showCommand(string);
+
+            }   // onClick
+        });
+    }
 
     private void showCommand(String textString) {
         Toast.makeText(MainActivity.this, textString, Toast.LENGTH_LONG).show();
